e446d8c3be9a555215ea27b36e0be249d421d3cf
kristi1705 <99876437+kristi1705@users.noreply.github.com>
2024-04-02 14:26:40 +0200

    Create authenticator_script.js

---
+++ authenticator_script.js
@@ -0,0 +1,18 @@
+  function switchForm(form) {
+    if (form === 'signup') {
+        document.getElementById('login-form').style.display = 'none';
+        document.getElementById('signup-form').style.display = 'block';
+    } else {
+        document.getElementById('signup-form').style.display = 'none';
+        document.getElementById('login-form').style.display = 'block';
+    }
+}
+
+// Implement your login and signup functions
+function login() {
+    // Login logic here
+}
+
+function signup() {
+    // Signup logic here
+}
